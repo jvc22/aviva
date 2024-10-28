0b2e56eadbfa4041dc939397c0a61d8d520566e4
joaovcostas <jvc22102003@gmail.com>
2024-10-28 13:35:15 -0300

    style: create blank project with font geist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        geist: ['var(--font-geist-sans)'],
+        'geist-mono': ['var(--font-geist-mono)'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
