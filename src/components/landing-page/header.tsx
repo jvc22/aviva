58eb8259d6464fbc7f6b6e44ea8668b5001e9d0a
jvc22 <joaovictorcostadasilva@outlook.com>
2024-10-31 19:37:40 -0300

    layout: apply respons.

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -6,10 +6,10 @@ import NavLink from './nav-link'
 
 export function Header() {
   return (
-    <header className="sticky top-0 flex items-center gap-8 px-8 py-4 backdrop-blur-sm">
+    <header className="sticky top-0 z-20 flex items-center gap-8 px-8 py-4 backdrop-blur-sm">
       <Image src="/logo.svg" width={92.75} height={16} alt="Logo da Aviva" />
 
-      <nav className="flex items-center gap-6">
+      <nav className="flex items-center gap-6 max-sm:hidden">
         <NavLink href="#start">Início</NavLink>
 
         <NavLink href="#platform">Plataforma</NavLink>
@@ -18,7 +18,7 @@ export function Header() {
       </nav>
 
       <div className="ml-auto flex items-center gap-2 text-sm">
-        <Button size="sm" variant="outline" className="dark">
+        <Button size="sm" variant="outline" className="dark max-md:hidden">
           Registrar
         </Button>
 
